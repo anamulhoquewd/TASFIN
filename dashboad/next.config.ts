c5d2c8977537ceac2f4102f1ee626530a47a52ea
Anamul <anamulhoquewd@gmail.com>
2025-08-23 19:03:59 +0600

    fix: create products with images (root images and variants images).

--- dashboad/next.config.ts
+++ dashboad/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["i.pinimg.com", "sg-products.s3.eu-north-1.amazonaws.com"],
+    domains: ["amazonaws.com"],
   },
 };
 
